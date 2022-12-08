a42851c7fb638b2eb8e505dbd8a5c9c124f12fa2
kde66034 <kde66034@gmail.com>
2022-12-08 17:44:38 +0900

    221208

--- assets/js3/horizontal.js
+++ assets/js3/horizontal.js
@@ -1,12 +1,25 @@
 gsap.registerPlugin(ScrollTrigger);
 
 const sections = gsap.utils.toArray(".section");
+const sectionsDesc = gsap.utils.toArray(".section__titleDescWrap");
 
 gsap.to(sections, {
     xPercent: -100 * (sections.length),
     ease: "none",
     scrollTrigger: {
         trigger: ".horizontalWrap",
+        pin: true,
+        scrub: 1,
+        end: "+=4000",
+        markers: false,
+    },
+});
+
+gsap.to(sectionsDesc, {
+    xPercent: -100 * (sectionsDesc.length),
+    ease: "none",
+    scrollTrigger: {
+        trigger: ".horizontalDescWrap",
         pin: true,
         scrub: 1,
         end: "+=4000",
