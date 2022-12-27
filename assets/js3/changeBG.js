520781cb89999e5f45cf843fe2f9015f4b55a5fa
kde66034 <kde66034@gmail.com>
2022-12-27 10:14:47 +0900

    221227

---
+++ assets/js3/changeBG.js
@@ -0,0 +1,27 @@
+gsap.utils.toArray(".backColor").forEach((elem) => {
+
+let color = elem.getAttribute('data-color');
+    
+ScrollTrigger.create({
+    trigger: elem,
+    start: 'top 20%',
+    end: 'bottom 20%',
+    markers: false,
+        onEnter: () => gsap.to('body', {
+            backgroundColor: color,
+            duration: 0.08
+        }),
+        onLeave: () => gsap.to('body', {
+            backgroundColor: '#FFFFFF',
+            duration: 0.08
+        }),
+        onLeaveBack: () => gsap.to('body', {
+            backgroundColor: '#FFFFFF',
+            duration: 0.08
+        }),
+        onEnterBack: () => gsap.to('body', {
+            backgroundColor: color,
+            duration: 0.08
+        }),
+    });
+});
